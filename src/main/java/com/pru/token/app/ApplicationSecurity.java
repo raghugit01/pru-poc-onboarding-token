0a4b85edd344ddc479e6697e680f6ca34bf82b0b
raghugit01 <biswalraghunath4@gmail.com>
2022-10-17 15:41:11 +0530

    cors disable
    cors disable

--- src/main/java/com/pru/token/app/ApplicationSecurity.java
+++ src/main/java/com/pru/token/app/ApplicationSecurity.java
@@ -41,6 +41,7 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
+		http.cors().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 		http.authorizeRequests()
