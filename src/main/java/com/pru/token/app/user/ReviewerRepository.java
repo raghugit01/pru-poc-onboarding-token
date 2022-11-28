fb3844624117b6552f7cf6d2f09192da3acf7275
Supriyawinisri1012 <shanmr17@in.ibm.com>
2022-11-28 12:44:51 +0530

    Test data

--- src/main/java/com/pru/token/app/user/ReviewerRepository.java
+++ src/main/java/com/pru/token/app/user/ReviewerRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface ReviewerRepository extends MongoRepository<Reviewer, String> {
 	
 	Reviewer findByEmpId(String empId);
+	
+	Reviewer findByReviewerName(String reviewerName);
 
 }
