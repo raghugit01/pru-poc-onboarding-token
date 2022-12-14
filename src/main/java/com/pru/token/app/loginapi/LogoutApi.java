e0be33828572e87da2d2f67c24bb8dd567f60766
raghugit01 <biswalraghunath4@gmail.com>
2022-12-14 13:40:21 +0530

    changing param
    Changing request parameter

--- src/main/java/com/pru/token/app/loginapi/LogoutApi.java
+++ src/main/java/com/pru/token/app/loginapi/LogoutApi.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pru.token.app.user.LogoutUserToken;
@@ -19,8 +20,8 @@ public class LogoutApi {
 	private LogoutUtil util;
 	
 	@PostMapping("/token")
-	public ResponseEntity<?> logout(@RequestBody String token){
-		boolean b = util.update(token);		
+	public ResponseEntity<?> logout(@RequestParam("token") String token){
+		boolean b = util.update(token);
 		return ResponseEntity.ok().body(b);
 	}
 	
